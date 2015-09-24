fdca0ce070843a87eca2e1484203584efb6eb629
lucassveloso <lucas.s.veloso@hotmail.com>
2015-09-24 16:04:46 -0300

    Refatorando build.gradle

--- src/main/java/biblioteca/app/Main.java
+++ src/main/java/biblioteca/app/Main.java
@@ -11,7 +11,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = {"biblioteca"})
 @EnableJpaRepositories(basePackages = {"biblioteca"})
 @EntityScan(basePackages = {"biblioteca"})
-
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
